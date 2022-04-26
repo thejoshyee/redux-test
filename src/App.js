afbb9dea05637fd4931c201352bfe74f0829984a
Josh Yee <joshyee@gmail.com>
2022-04-25 18:51:26 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import noteReducer from './reducers/noteReducer'
 
 import { createStore } from 'redux'
 
-const noteReducer = (state = [], action) => {
-  switch(action.type) {
-    case 'NEW_NOTE':
-      return state.concat(action.data)
-    case 'TOGGLE_IMPORTANCE': {
-      const id = action.data.id
-      const noteToChange = state.find(n => n.id === id)
-      const changedNote = { 
-        ...noteToChange, 
-        important: !noteToChange.important 
-      }
-      return state.map(note =>
-        note.id !== id ? note : changedNote 
-      )
-     }
-    default:
-      return state
-  }
-}
 
 const store = createStore(noteReducer)
 
