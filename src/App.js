9622246ffdfd145b8030dca507fb24dadea02a7d
Josh Yee <joshyee@gmail.com>
2022-04-28 16:08:13 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
-   
+import { useEffect } from 'react'
+
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 
+import { initializeNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
+
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(initializeNotes()) 
+  },[dispatch]) 
   
   return (
     <div>
